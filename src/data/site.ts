4c37865e4e2624c40a11293bc63024dcde278721
pcmsomagede <pcmsomagede@gmail.com>
2026-08-16 23:15:23 +0700

    feat: add organization profile data

---
+++ src/data/site.ts
@@ -0,0 +1,21 @@
+export const site = {
+  name: 'Pimpinan Cabang Muhammadiyah Somagede Kabupaten Banyumas',
+  shortName: 'PC Muhammadiyah Somagede',
+  chair: 'Drs. Bambang Budiarso',
+  address: 'Masjid Darul Arqom Komplek SMK Muhammadiyah Somagede, Jl. Raya Somagede Km. 5, Kode Pos 53193',
+  whatsapp: ['081542889595', '081391329139'],
+  email: '',
+  url: 'https://pcmsomagede.github.io',
+  description: 'Website resmi Pimpinan Cabang Muhammadiyah Somagede Kabupaten Banyumas.',
+};
+
+export const navItems = [
+  { label: 'Beranda', href: '/' },
+  { label: 'Berita', href: '/berita' },
+  { label: 'Kegiatan', href: '/kegiatan' },
+  { label: 'Agenda', href: '/agenda' },
+  { label: 'Organisasi', href: '/organisasi' },
+  { label: 'Galeri', href: '/galeri' },
+  { label: 'Arsip', href: '/arsip' },
+  { label: 'Kontak', href: '/kontak' },
+];
