263f046bb9f73e8be17f5e832a05e46b46aa0c66
pcmsomagede <pcmsomagede@gmail.com>
2026-08-17 22:45:10 +0700

    fix: use supplied hero asset and remove chair from site header data

--- src/data/site.ts
+++ src/data/site.ts
@@ -4,13 +4,12 @@ export const sitePath = (path = '/') => `${base.replace(/\/$/, '')}${path.starts
 export const site = {
   name: 'Pimpinan Cabang Muhammadiyah Somagede Kabupaten Banyumas',
   shortName: 'PC Muhammadiyah Somagede',
-  chair: 'Drs. Bambang Budiarso',
   address: 'Masjid Darul Arqom Komplek SMK Muhammadiyah Somagede, Jl. Raya Somagede Km. 5, Kode Pos 53193',
   whatsapp: ['081542889595', '081391329139'],
   email: '',
   url: 'https://pcmsomagede.github.io/',
   description: 'Website resmi Pimpinan Cabang Muhammadiyah Somagede Kabupaten Banyumas sebagai pusat berita, kegiatan, agenda, dokumentasi, dan arsip digital.',
-  heroImage: 'https://res.cloudinary.com/v6hqki7m/image/upload/f_auto,q_auto,w_1600/profil-1-1024x490',
+  heroImage: '/hero.webp',
 };
 
 export const navItems = [
